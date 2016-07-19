bc000eee514ad052a1650a18cf2ddd10ee53884e
aprilsacil <asacil@openovate.com>
2016-07-19 15:23:41 +0800

    seller award modal integration

--- app/pages/seller-award-modal/seller-award-modal.ts
+++ app/pages/seller-award-modal/seller-award-modal.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { Alert, Loading, NavController, Toast, ViewController } from 'ionic-angular';
+import { HTTP_PROVIDERS, Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import { LocalStorageProvider } from '../../providers/storage/local-storage-provider';
+import 'rxjs/add/operator/map';
+
+var PouchDB = require('pouchdb');
+PouchDB.plugin(require('pouchdb-authentication'));
+PouchDB.plugin(require('pouchdb-quick-search'));
 
 /*
   Generated class for the SellerAwardModalPage page.
@@ -9,14 +17,33 @@ import { Alert, Loading, NavController, Toast, ViewController } from 'ionic-angu
 */
 @Component({
   templateUrl: 'build/pages/seller-award-modal/seller-award-modal.html',
+  providers: [HTTP_PROVIDERS, LocalStorageProvider]
 })
 export class SellerAwardModalPage {
-    award: Object = {};
+    private db;
+    award = {
+        purchasedItem: <string>  null,
+        price: <string> null,
+        message: <string> null,
+        username: <string> null,
+        store: <string> null
+    };
 
     constructor(
+        private localStorage: LocalStorageProvider,
         private nav: NavController,
+        private http: Http,
         private view: ViewController
-    ) {}
+    ) {
+        // couch db integration
+        this.db = new PouchDB('http://localhost:5984/cheers', {skipSetup: true});
+
+        // local integration
+        var local = new PouchDB('cheers');
+
+        // this will sync locally
+        local.sync(this.db, {live: true, retry: true}).on('error', console.log.bind(console));
+    }
 
     /**
      * Closes the modal
@@ -54,20 +81,37 @@ export class SellerAwardModalPage {
         // render
         this.nav.present(loading);
 
-        // TODO: add thingy here
-        setTimeout(() => {
-            // dismiss the loader
-            loading.dismiss().then(() => {
-                // close the modal
-                this.dismiss();
-            })
-            .then(() => {
-                // delay it for a second
-                setTimeout(() => {
-                    // show a toast
-                    this.showToast('You have successfully gave the customer an award.');
-                }, 400);
-            });
-        }, 3000);
+        this.localStorage.getFromLocal('user').then((data) => {
+            var user = JSON.parse(data);
+
+            let headers = new Headers({
+                'Content-Type': 'application/x-www-form-urlencoded'});
+
+            var param = this.award;
+            //TODO :: make it dynamic
+            param.username = 'aprilsacil';
+            param.store = user.name;
+
+            this.http
+                .post('http://cheers.dev/history', param, {headers: headers})
+                .map(response => response.json())
+                .subscribe((data) => {
+                    if(data.ok) {
+                        loading.dismiss().then(() => {
+                            // close the modal
+                            this.dismiss();
+                        })
+                        .then(() => {
+                            // delay it for a second
+                            setTimeout(() => {
+                                // show a toast
+                                this.showToast('You have successfully gave the customer an award.');
+                            }, 400);
+                        });
+                    }
+                }, (error) => {
+                    console.log(error);
+                });
+        });
     }
 }
