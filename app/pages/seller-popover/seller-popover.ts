832819038a173f10b993d02a17f451d6a47e58e9
Ruje Alfon <parengruje@gmail.com>
2016-07-29 18:04:14 +0800

    generate seller account settings page and popover page

---
+++ app/pages/seller-popover/seller-popover.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the SellerPopoverPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/seller-popover/seller-popover.html',
+})
+export class SellerPopoverPage {
+
+  constructor(private nav: NavController) {
+
+  }
+
+}
