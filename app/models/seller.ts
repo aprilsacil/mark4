2577e066334b4d25eb7c4b880dcc7016ffc7ef05
Ruje Alfon <parengruje@gmail.com>
2016-08-01 20:15:12 +0800

    added store image

--- app/models/seller.ts
+++ app/models/seller.ts
@@ -3,6 +3,7 @@ export class Seller {
     name: string;
     fullname: string;
     store_uuid: string;
+    store_image: string = null;
     image: string = null;
     roles: string;
     emote_message: string = null;
@@ -27,6 +28,11 @@ export class Seller {
         // image
         if (userInfo.image) {
             this.image = userInfo.image;
+        }
+
+        // store_image
+        if (userInfo.store_image) {
+            this.store_image = userInfo.store_image;
         }
 
         // store_uuid
