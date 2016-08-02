3314eed20059729769578ac057de8c62a98afbb8
aprilsacil <asacil@openovate.com>
2016-08-02 15:55:08 +0800

    buyer level and and award

--- app/pages/relogin/relogin.ts
+++ app/pages/relogin/relogin.ts
@@ -139,6 +139,7 @@ export class ReloginPage {
                 }
 
                 var user = data.user;
+                user.level = Math.floor((Math.sqrt(user.experience / 15) / 2));
 
                 // set the timestamp
                 self.localStorage.setToLocal('timestamp', Math.round(new Date().getTime()/1000));
