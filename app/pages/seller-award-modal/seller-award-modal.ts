15b736a18d6ed5416140db05646263569abdcb28
aprilsacil <asacil@openovate.com>
2016-08-02 16:06:53 +0800

    initialized store_image and seller

--- app/pages/seller-award-modal/seller-award-modal.ts
+++ app/pages/seller-award-modal/seller-award-modal.ts
@@ -31,7 +31,9 @@ export class SellerAwardModalPage {
         message: <string> null,
         username: <string> null,
         store: <string> null,
-        image: <string> null
+        seller: <string> null,
+        image: <string> null,
+        store_image: <string> null
     };
     shopper: any;
     user = new Seller({});
