6124cece2f2d6116754c090345bd81cbf4b171af
Ruje Alfon <parengruje@gmail.com>
2016-08-02 15:57:25 +0800

    registration id on login

--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -27,7 +27,8 @@ import 'rxjs/add/operator/map';
 export class LoginPage {
     login = {
         username: <string> null,
-        password: <string> null
+        password: <string> null,
+        registration_id: null
     };
     goBack = false;
 
@@ -41,6 +42,11 @@ export class LoginPage {
         @Inject('APIEndpoint') private apiEndpoint: string
     ) {
         this.goBack = this.params.get('go_back') || false;
+
+        // get registration id
+        this.localStorage.getFromLocal('registration_id').then((id) => {
+            this.login.registration_id = id || '';
+        });
     }
 
     /**
